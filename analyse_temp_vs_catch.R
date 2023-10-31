260b10dbb3f5a2da21bd19767d5a8b6485eab05c
James <jamesmaino@gmail.com>
2023-11-01 10:47:35 +1100

    update temperature vs catch plot

--- analyse_temp_vs_catch.R
+++ analyse_temp_vs_catch.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 source("./utils/get_silo_data.R")
 source("./utils/load_trap_data.R")
+source("./utils/calc_hourly_temp.R")
 
-d <- load_trap_data()
+
+# load traps but only include SA and NSW traps
+d <- load_trap_data() %>%
+    filter(loc %in% c("Lareldale, Armidale", "Point Lookout", "Mt. Dowe", "Kingstown", "Newholme", "Thora", "Turretfield"))
 
 dsum <- d %>%
     group_by(loc, lon, lat) %>%
@@ -21,14 +25,26 @@ climate <- dsum %>%
     transmute(loc, lon, lat, date = list(silo$`YYYY-MM-DD`), min_temp = list(silo$min_temp), max_temp = list(silo$max_temp)) %>%
     unnest(cols = c(date, min_temp, max_temp))
 
+# offset date by one day to get previous day temp
+climate <- climate %>%
+    mutate(date = date - 1)
+
 df <- d %>%
     left_join(climate) %>%
-    select(loc, daily_count, min_temp, max_temp)
+    select(loc, daily_count, min_temp, max_temp) %>%
+    rowwise() %>%
+    mutate(temp8pm = calc_hourly_temp(min_temp, max_temp, hour = 20)) %>%
+    mutate(temp_round = round(temp8pm)) %>%
+    group_by(loc, temp_round) %>%
+    summarise(total_count = sum(daily_count, na.rm = TRUE)) %>%
+    arrange(loc, temp_round) %>%
+    group_by(loc) %>%
+    mutate(total_count_cumulative = cumsum(total_count))
 
 df %>%
-    ggplot() +
-    geom_point(aes(max_temp, daily_count, color = loc)) +
-    scale_y_log10() +
+    ggplot(aes(temp_round, total_count_cumulative, color = loc)) +
+    geom_point() +
+    geom_line(alpha = 0.5) +
     guides(color = "none") +
     facet_wrap(~loc, scales = "free")
 ggsave("./results/temperature_vs_daily_catch.png", width = 10, height = 6)
